4252413fc1e0c52d12986702b33ebafda6f393df
Tyranozomby <tyranozomby@gmail.com>
2021-12-01 19:56:03 +0100

    petits trucs

--- CardGameEngine/Cards/CardPiles/CardPile.cs
+++ CardGameEngine/Cards/CardPiles/CardPile.cs
@@ -1,11 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CardGameEngine.Cards.CardPiles
 {
-    public class CardPile : IEnumerable
+    public class CardPile : IEnumerable<Card>
     {
         private List<Card> cardList;
+
+
+        //Permet de faire pile[i] comme si c'était un tableau normal
+        //Oui on peut changer ça en c#. Et les opérateurs aussi, genre + et -
+        public Card this[int i] => cardList.ElementAt(i);
+
+        IEnumerator<Card> IEnumerable<Card>.GetEnumerator()
+        {
+            //TODO
+            throw new System.NotImplementedException();
+        }
 
         public IEnumerator GetEnumerator()
         {
