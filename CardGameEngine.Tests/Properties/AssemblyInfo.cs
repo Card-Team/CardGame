671b1534c9ea2c7a9b28dd874f227aa44a32702f
Bilel MEDIMEGH <bilel.medimegh@gmail.com>
2021-12-06 22:44:15 +0100

    Rename test project

--- CardGameEngine.Tests/Properties/AssemblyInfo.cs
+++ CardGameEngine.Tests/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CardGameTests")]
+[assembly: AssemblyTitle("CardGameEngine.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CardGameTests")]
+[assembly: AssemblyProduct("CardGameEngine.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
