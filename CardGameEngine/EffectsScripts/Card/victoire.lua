2d851c620a1a60d36703942621e849a0c368da63
Stephane <stephanelay78370@gmail.com>
2021-12-12 18:33:06 +0100

    Etablissement des cartes victoire et visionnaire (Non Finie)

---
+++ CardGameEngine/EffectsScripts/Card/victoire.lua
@@ -0,0 +1,22 @@
+﻿max_level = 10 --TODO--
+image_id = 519
+
+
+name = "Victoire"
+pa_cost = 1
+
+-- fonction qui renvoie un booléen si la carte peut être jouée ou non
+function precondition()
+    return true
+end
+
+function description()
+    return "Plus que ".. max_level-current_level .."évolution de la carte pour terminer la partie"
+end
+
+function do_effect()
+end
+
+function on_level_change(oldLevel, newLevel)
+    -- fonction appelée quand la carte change de niveau (OPTIONNEL)
+end
---
+++ CardGameEngine/EffectsScripts/Card/visionnaire.lua
@@ -0,0 +1,24 @@
+﻿--immutable data
+max_level = 3
+image_id = 518
+
+--mutable data
+name = "Visionnaire"
+pa_cost = 2
+targets = {}
+
+
+function precondition()
+   
+end
+
+function description()
+  
+end
+
+function do_effect()
+    
+end
+
+
+--todo CreateTarget, AskForTarget(target,predicate for card),currentLevel,TargetTypes,TargetsExists,EffectOwner
